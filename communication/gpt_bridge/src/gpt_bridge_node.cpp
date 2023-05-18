fca26b001bb1f14241921989e88f7777149adf6f
LeoMuuuu <mutongtongxue@yeah.net>
2023-05-18 19:31:44 -0400

    gpt_bridge change for moveit

--- communication/gpt_bridge/src/gpt_bridge_node.cpp
+++ communication/gpt_bridge/src/gpt_bridge_node.cpp
@@ -2,7 +2,10 @@
 #include <std_msgs/msg/string.hpp>
 #include <chrono>
 #include <thread>
-
+#include "std_msgs/msg/float64_multi_array.hpp"
+#include <sensor_msgs/msg/joint_state.hpp>
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include <string>
 class GPTBridgeNode : public rclcpp::Node {
 public:
   GPTBridgeNode()
@@ -33,41 +36,130 @@ private:
   }
 
   void execute_command(const std::string& command) {
-    if (command.find("adjust_target") != std::string::npos) {
-      std::string target;
-      std::vector<float> coordinates(3);
-      sscanf(command.c_str(), "adjust_target:%[^;];[%f %f %f]", target.data(), &coordinates[0], &coordinates[1], &coordinates[2]);
-      RCLCPP_INFO(this->get_logger(), "Adjusting target %s to coordinates [%f, %f, %f]", target.c_str(), coordinates[0], coordinates[1], coordinates[2]);
+    if (command.find("reposition_robot") != std::string::npos) {
+      RCLCPP_INFO(this->get_logger(), "Repositioning robot");
       
-      // Add code to execute adjust_target command
-      ///////////////////////////////////////////////////////////////////////////////////
-      std::this_thread::sleep_for(std::chrono::seconds(3));
-      ///////////////////////////////////////////////////////////////////////////////////
+      rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("move_robot/move_robot_fnf", 10000);
+            auto message = geometry_msgs::msg::PoseStamped();
 
-    } else if (command == "move_robot") {
-      RCLCPP_INFO(this->get_logger(), "Moving robot");
+        // Update the message header
+        message.header.frame_id = "base_link";  // or whatever frame you're using
 
-      // Add code to execute move_robot command
-      ///////////////////////////////////////////////////////////////////////////////////
-      std::this_thread::sleep_for(std::chrono::seconds(3));
-      ///////////////////////////////////////////////////////////////////////////////////
+        // Fill in pose data  -0.3508 0.00 0.1305
+        message.pose.position.x = -0.3508;
+        message.pose.position.y = 0.00;
+        message.pose.position.z = 0.1305;
 
+        message.pose.orientation.x = 0.0;
+        message.pose.orientation.y = 1.0;
+        message.pose.orientation.z = 0.0;
+        message.pose.orientation.w = 0.0;
+
+        // Finally, publish the message
+        RCLCPP_INFO(this->get_logger(), "Publishing PoseStamped");
+        publisher_->publish(message);
+        /*
+    auto pub = this->create_publisher<std_msgs::msg::Float64MultiArray>("/ur5_controller/commands", 10);
+    // Create a message
+    std_msgs::msg::Float64MultiArray msg;
+    // Fill the message
+    msg.layout.dim = {}; // Empty as per your example
+    msg.layout.data_offset = 0;
+    msg.data = {0.25, -1.5, -1.7, -1.0, 1.5, 0.3, -0.3, -0.3};
+    // Publish the message
+    pub->publish(msg);
+    */
+
+      ///////////////////////////////////////////////////////////////////////////////////
+    } else if (command.find("move_robot") != std::string::npos) {
+      ////////////////////////target_positions//////////////////////////
+      //////////////blue ball -0.5308 0.00 0.0305///////////////////////
+      //////////////red ball: -0.5308 0.2368 0.0305/////////////////////
+      //////////////green box: -0.5308 -0.2368 0.0305///////////////////
+      std::string target;
+      sscanf(command.c_str(), "move_robot: %s\n", target.data());
+      rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("move_robot/move_robot_fnf", 10000);
+            auto message = geometry_msgs::msg::PoseStamped();
+
+        // Update the message header
+        message.header.frame_id = "base_link";  // or whatever frame you're using
+
+        // Fill in pose data  -0.3508 0.00 0.1305
+        message.pose.position.x = -0.3508;
+        message.pose.position.y = 0.00;
+        message.pose.position.z = 0.1305;
+
+        message.pose.orientation.x = 0.0;
+        message.pose.orientation.y = 1.0;
+        message.pose.orientation.z = 0.0;
+        message.pose.orientation.w = 0.0;
+
+        std::string str1(target.c_str());
+        std::string strg("green_box");
+        std::string strb("blue_ball");
+        std::string strr("red_ball");
+          if(str1.compare(strg)==0){
+            RCLCPP_INFO(this->get_logger(), "Begin to move the green box...");
+            message.pose.position.x = -0.5308;
+            message.pose.position.y = -0.2368;
+            message.pose.position.z =  0.0305;
+          }
+          else if(str1.compare(strb)==0){
+            RCLCPP_INFO(this->get_logger(), "Begin to move the blue ball...");
+            message.pose.position.x = -0.5308;
+            message.pose.position.y = 0.00;
+            message.pose.position.z =  0.0305;
+          }
+          else if(str1.compare(strr)==0){
+            RCLCPP_INFO(this->get_logger(), "Begin to move the red ball...");
+            message.pose.position.x = -0.5308;
+            message.pose.position.y =  0.2368;
+            message.pose.position.z =  0.0305;
+          }
+        
+        publisher_->publish(message);
+        std::this_thread::sleep_for(std::chrono::seconds(10));
     } else if (command.find("move_gripper") != std::string::npos) {
+      /*
       int open;
       sscanf(command.c_str(), "move_gripper:%d", &open);
       RCLCPP_INFO(this->get_logger(), "Moving gripper: %s", open ? "1" : "0");
+      auto parameters_client = this->create_client<rcl_interfaces::srv::SetParameters>("/rtt/set_parameters");
+      while (!parameters_client->wait_for_service(std::chrono::seconds(1))) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+                return;
+            }
+            RCLCPP_INFO(this->get_logger(), "Waiting for service to become available...");
+        }
+        auto request = std::make_shared<rcl_interfaces::srv::SetParameters::Request>();
+        rcl_interfaces::msg::Parameter left_g;
+        rcl_interfaces::msg::Parameter right_g;
+        left_g.name = "Control/left_gripper";
+        right_g.name = "Control/right_gripper";
+        if(open==1){
+        left_g.value.double_value = 0.2;
+        right_g.value.double_value = 0.2;
+        }
+        else if(open==0){
+        left_g.value.double_value = -0.3;
+        right_g.value.double_value = -0.3;
+        }
+        left_g.value.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
+        right_g.value.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
+        request->parameters.push_back(left_g);
+        request->parameters.push_back(right_g);
+        auto result_future = parameters_client->async_send_request(request);
 
-      // Add code to execute move_gripper command
-      ///////////////////////////////////////////////////////////////////////////////////
-      std::this_thread::sleep_for(std::chrono::seconds(3));
-      ///////////////////////////////////////////////////////////////////////////////////
-      
+        std::this_thread::sleep_for(std::chrono::seconds(8));
+        */
     } else {
       RCLCPP_WARN(this->get_logger(), "Unknown command: %s", command.c_str());
     }
   }
 
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
 };
 
 int main(int argc, char *argv[]) {
--- communication/gpt_ros/gpt_ros/gpt_config.py
+++ communication/gpt_ros/gpt_ros/gpt_config.py
@@ -4,7 +4,7 @@ class GPTConfig:
         # self.model = "gpt-4"
         self.api_key = "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"
         self.organization = "JohnsHopkins"
-        self.temperature = 0.1
+        self.temperature = 0.4
         self.max_tokens = 3000
         self.top_p = 1
         self.frequency_penalty = 0
@@ -12,26 +12,49 @@ class GPTConfig:
         self.stop = [" You:", " Assistant:"]
         self.system_prompt =\
         """You're a core of a ROS2 based UR-5 robot, and you need to convert the input text to a programmatic command, there're three operations: 
-1. move_robot ( no arguments, this command will move the robot to the 'target_pos')
+1. move_robot ( argument: target name, this command will move the robot to the configuration of the target) you can only output the targets in the list after 'move_robot' text, you can reagrd the synonyms as the same object:
+
+list: green_box red_ball blue_ball
+
 2. move_gripper ( argument: bool  1 for close the gripper and 0 for open the gripper ) 
-3. adjust_target ( argument1: parameter_name a string which represents the target objecct; argument2: the change of the parameter, which should be a 1*3 vector,[dx dy dz]); +dx : move forward, -dx : move back, +dy : move right, -dy move left, +dz: move up, -dz: move down. When you want to move a box, pick it and lift it up (dz=+1), then move to the target x or y, then move down (dz=-1).
+3. reposition_robot ( no argument, this command will move the robot to the origin pose)
 
-Your output command should strictly follow this format, don't output any other words or explanations:
-COMMAND_NAME:ARGUMENT1
-COMMAND_NAME:ARGUMENT1;ARGUMENT2
+Your output command should strictly follow this format, don't output any other words or explanations, replace the space in the object with '_', to pick an object up, you should reposition the robot when the gripper is close:
+COMMAND_NAME
+COMMAND_NAME:ARGUMENT
 
-Example: I want to move the blue box to the right of the red box
-Output: 
-adjust_target:blue_box;[0 0 0]
-move_robot
+Example 1:  pick up the red ball
+
+reposition_robot
+move_gripper:0
+move_robot:red_ball
 move_gripper:1
-adjust_target:blue_box;[0 0 1]
-move_robot
-adjust_target:red_box;[0 1 1]
-move_robot
-adjust_target:red_box;[0 1 0]
-move_robot
-move_gripper:0"""
+reposition_robot
+
+Example 2: Put back the green box
+ 
+move_robot:green_box
+move_gripper:0
+reposition_robot
+
+Example 3: drop it down
+
+move_gripper:0
+
+Example 4:  pick up the red sphere for me
+
+reposition_robot
+move_gripper:0
+move_robot:red_ball
+move_gripper:1
+reposition_robot
+
+Example 5:  pick up the yellow box for me
+
+reposition_robot
+move_gripper:0
+
+"""
         self.user_prompt = "default prompt"
         self.assisstant_response = "default response"
         self.chat_history = [{"role": "system", "content": self.system_prompt}]
